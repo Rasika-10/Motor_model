ce115240387fb96cb576321e080c67446cee5bc8
Rasika-7 <727721euit126@skcet.ac.in>
2023-12-20 01:52:11 +0530

    added dep array

--- src/components/xr-hit-model/Model.js
+++ src/components/xr-hit-model/Model.js
@@ -1,80 +1,3 @@
-
-
-// import React, { useRef, useState } from "react";
-// import { useGLTF} from "@react-three/drei";
-// import * as THREE from 'three';
-
-// export default function Model(props) {
-//   const group = useRef();
-//   const { nodes, materials } = useGLTF("/electric_motor.glb");
-//   // const { actions } = useAnimations(animations, group);
-
-//   const [temperature] = useState(20); // Initial temperature
-
-//   // Set up materials with enhanced shading properties
-//   materials.parts.color.set("0xffffff"); // Change to white
-//   materials.closingParts.color.set("#0000ff"); // Change to black
-
-//   // Adjust shading properties for a more vibrant 3D effect
-//   materials.parts.metalness = 0.8; // Increase metalness for a shiny effect
-//   materials.parts.roughness = 0.1; // Decrease roughness for a glossy effect
-
-//   materials.closingParts.metalness = 0.8;
-//   materials.closingParts.roughness = 0.1;
-
-//   // Function to update the temperature and simulate overheating
-  
-
-//   // Function to determine color based on temperature
-//   const getBodyColor = () => {
-//     if (temperature < 50) {
-//       return new THREE.Color(0.6, 0.8, 1).multiplyScalar(3); // Enhance blue color
-//     } else if (temperature >= 50 && temperature <= 100) {
-//       const t = (temperature - 50) / 50; // Normalize temperature between 50 and 100
-//       return new THREE.Color(0.9, t * 0.5, 0).multiplyScalar(1.5); // Enhance red color
-//     } else {
-//       // Dark red with orange and yellow tones, highlighted
-//       return new THREE.Color(1, 0, 0).multiplyScalar(1.2).add(new THREE.Color(1, 0.9, 0.5).multiplyScalar(0.6));
-//     }
-//   };
-
-//   // Set up the body material with dynamic color based on temperature
-//   materials.body.color.copy(getBodyColor());
-
-//   return (
-//     <group ref={group} {...props} dispose={null}>
-//       <group rotation={[-Math.PI / 2, 0, 0]}>
-//         <group rotation={[Math.PI / 2, 0, 0]}>
-//           {/* Motor model */}
-//           <mesh
-//             castShadow
-//             receiveShadow
-//             geometry={nodes.defaultMaterial.geometry}
-//             material={materials.body}
-           
-//           />
-//           <mesh
-//             castShadow
-//             receiveShadow
-//             geometry={nodes.defaultMaterial_1.geometry}
-//             material={materials.parts}
-//           />
-//           <mesh
-//             castShadow
-//             receiveShadow
-//             geometry={nodes.defaultMaterial_2.geometry}
-//             material={materials.closingParts}
-//           />
-//         </group>
-//       </group>
-//     </group>
-//   );
-// }
-
-// useGLTF.preload("/electric_motor.glb");
-
-
-
 import React, { useRef, useState, useEffect } from "react";
 import { useGLTF } from "@react-three/drei";
 import * as THREE from 'three';
@@ -84,13 +7,8 @@ import { ref,onValue } from 'firebase/database'
 export default function Model(props) {
 
   const [data, setData] = useState([]);
-
-
   const group = useRef();
   const { nodes, materials } = useGLTF("/electric_motor.glb");
-
-  // const [temperature] = useState(70); // Initial temperature
-
   const [temperature, setTemperature]=useState('20');
   const [changer, setChanger]=useState(0);
 
@@ -153,7 +71,7 @@ export default function Model(props) {
     
   );
 
-},[changer]);
+},[changer,data]);
 
 
 
