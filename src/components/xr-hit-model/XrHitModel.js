a5218bc7e8f94c78992875f7ff4824d5888cd161
Rasika-7 <727721euit126@skcet.ac.in>
2023-12-20 10:33:25 +0530

    first commit

--- src/components/xr-hit-model/XrHitModel.js
+++ src/components/xr-hit-model/XrHitModel.js
@@ -1,8 +1,69 @@
+// import { OrbitControls } from "@react-three/drei";
+// import { useThree } from "@react-three/fiber";
+// import { Interactive, useHitTest, useXR } from "@react-three/xr";
+// import { useRef, useState } from "react";
+// import Model from "./Model";
+
+// const XrHitModel = () => {
+//   const reticleRef = useRef();
+//   const [models, setModels] = useState([]);
+
+//   const { isPresenting } = useXR();
+
+//   useThree(({ camera }) => {
+//     if (!isPresenting) {
+//       camera.position.z = 3;
+//     }
+//   });
+
+//   useHitTest((hitMatrix, hit) => {
+//     hitMatrix.decompose(
+//       reticleRef.current.position,
+//       reticleRef.current.quaternion,
+//       reticleRef.current.scale
+//     );
+
+//     reticleRef.current.rotation.set(-Math.PI / 2, 0, 0);
+//   });
+
+//   const placeModel = (e) => {
+//     let position = e.intersection.object.position.clone();
+//     let id = Date.now();
+//     setModels([{ position, id }]);
+//   };
+
+//   return (
+//     <>
+//       <OrbitControls />
+//       <ambientLight />
+//       {isPresenting &&
+//         models.map(({ position, id }) => {
+//           return <Model key={id} position={position} />;
+//         })}
+//       {isPresenting && (
+//         <Interactive onSelect={placeModel}>
+//           <mesh ref={reticleRef} rotation-x={-Math.PI / 2}>
+//             <ringGeometry args={[0.1, 0.25, 32]} />
+//             <meshStandardMaterial color={"white"} />
+//           </mesh>
+//         </Interactive>
+//       )}
+
+//       {!isPresenting && <Model />}
+//     </>
+//   );
+// };
+
+// export default XrHitModel;
+
+
+
 import { OrbitControls } from "@react-three/drei";
 import { useThree } from "@react-three/fiber";
 import { Interactive, useHitTest, useXR } from "@react-three/xr";
 import { useRef, useState } from "react";
 import Model from "./Model";
+import { Text } from '@react-three/drei';
 
 const XrHitModel = () => {
   const reticleRef = useRef();
@@ -38,7 +99,22 @@ const XrHitModel = () => {
       <ambientLight />
       {isPresenting &&
         models.map(({ position, id }) => {
-          return <Model key={id} position={position} />;
+          return (
+            <group key={id} position={position}>
+              <Model />
+              <Text
+                position={[0, 1, 0]} // Adjust the position on the motor model
+                color="black"
+                fontSize={2}
+                maxWidth={2}
+                lineHeight={1}
+                letterSpacing={0.02}
+                textAlign="center"
+              >
+                Aurora
+              </Text>
+            </group>
+          );
         })}
       {isPresenting && (
         <Interactive onSelect={placeModel}>
